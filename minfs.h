c22a3eb941f952742328cb4448af08a1a3df9ba4
Ryan Kruger <rtkruger@calpoly.edu>
2018-12-07 22:15:32 -0800

    changed a ton, mainly partitioning.

--- minfs.h
+++ minfs.h
@@ -5,7 +5,24 @@
 #define INODE_SIZE 64
 #define MINIX_MAGIC 0x4D5A
 #define DIRECT_ZONES 7
+#define P_TABLE_LOCATION 0x1BE
+#define MINIX_PARTITION_TYPE 0x81
+#define TABLE_SIGNATURE 0x55aa
+#define SECTOR_SIZE 512
 
+struct partition_table { /* A Minix Partition table */
+   
+   uint8_t bootind; /* Boot magic number (0x80 if bootable. */
+   uint8_t start_head; /* Start of partition in CHS */
+   uint8_t start_sec;
+   uint8_t start_cyl;
+   uint8_t type; /* Type of partition (0x81 for MINIX) */
+   uint8_t end_head; /* End of partition in CHS */
+   uint8_t end_sec;
+   uint8_t end_cyl;
+   uint32_t lFirst; /* First LBA sector. */
+   uint32_t size; /* Size of partition (in sectors). */
+};
 
 struct superblock { /* Minix Version 3 Superblock from fs/super.h */
 
@@ -35,7 +52,7 @@ struct inode { /* Sucture of a minix inode on disk */
     int32_t mtime;
     int32_t ctime;
     uint32_t zone[DIRECT_ZONES];
-    uint32_t indrect;
+    uint32_t indirect;
     uint32_t two_indirect;
     uint32_t unused;
 };
@@ -44,7 +61,12 @@ struct inode { /* Sucture of a minix inode on disk */
 
 void handle_args(int argc, char **arg);
 FILE *file_open_read(char *filename);
-void handle_partitions();
+int handle_partitions();
 struct superblock validate_superblock(FILE *imagefile);
+struct partition_table validate_p_table(FILE *imagefile);
+void printSecondsToReadable(int32_t time);
+void printModeBits(uint16_t mode);
+
+
 
 #endif
--- minls.c
+++ minls.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv) {
    int file_found = 0;
    int path_size;
    int block_index;
+   int partition_offset = 0;
    fpos_t *pos;
    struct inode current_inode;
 
@@ -30,7 +31,7 @@ int main(int argc, char **argv) {
    
    /* TODO: Handle file partitions if needed */
    if(part) {
-      handle_partitions();
+      partition_offset = handle_partitions(imagefile);
    }
    
    /* For now, assuming there is no partitions */
@@ -61,8 +62,8 @@ int main(int argc, char **argv) {
    zone_size = blocksize << s_block.log_zone_size;
    inode_map_size = s_block.i_blocks * blocksize;
    zone_map_size = s_block.z_blocks * blocksize;
-   inode_table_addr = (SUPERBLOCK_ADDR * 2) + inode_map_size + zone_map_size;v // Was wrong, do we still need?
-   inode_table_size = s_block.ninodes * INODE_SIZE / blocksize;
+   inode_table_addr = (SUPERBLOCK_ADDR * 2) + inode_map_size + zone_map_size; // Was wrong, do we still need?
+   inode_table_size = s_block.ninodes * INODE_SIZE / blocksize; // removE?
    block_index = 2+s_block.i_blocks+s_block.z_blocks;
 
    //TODO print calculated values?
@@ -91,17 +92,17 @@ int main(int argc, char **argv) {
    if(verbose) {
       printf("\n");
       printf("File inode:\n");
-      printf("  uint16_t mode           0x%4x ",current_inode.mode);
+      printf("  uint16_t mode           0x%4x \n",current_inode.mode);
       printModeBits(current_inode.mode);
       printf("  uint16_t links %15u\n",current_inode.links);
       printf("  uint16_t uid %17u\n",current_inode.uid);
       printf("  uint16_t gid %17u\n",current_inode.gid);
       printf("  uint32_t size %16u\n",current_inode.size);
-      printf("  uint32_t atime %15d --- ",current_inode.atime);
+      printf("  uint32_t atime %15d --- \n",current_inode.atime);
       printSecondsToReadable(current_inode.atime);
-      printf("  uint32_t mtime %15d --- ",current_inode.mtime);
+      printf("  uint32_t mtime %15d --- \n",current_inode.mtime);
       printSecondsToReadable(current_inode.mtime);
-      printf("  uint32_t ctime %15d --- ",current_inode.ctime);
+      printf("  uint32_t ctime %15d --- \n",current_inode.ctime);
       printSecondsToReadable(current_inode.ctime);
       printf("\n");
    }
@@ -134,7 +135,7 @@ void handle_args(int argc, char **argv) {
    extern int optind;
    int c, error = 0;
    int pflag = 0, sflag = 0;
-   static char usage[] = "usage: %s [-v] [-p part [-s subpart]] imagefile [path]";
+   static char usage[] = "usage: %s [-v] [-p part [-s subpart]] imagefile [path]\n";
 
    while((c = getopt(argc, argv, "vp:s:")) != -1)
       switch(c) {
@@ -192,9 +193,91 @@ FILE *file_open_read(char *filename) {
 
    
 /* Called if partitioning is requested */
-void handle_partitions() {
-      
+int handle_partitions(FILE *imagefile) {
+   int part_num = -1;
+   int subpart_num = -1;
+   int position_offset;
+   struct partition_table p_table;
+
+   /* First, check for a valid partition table. */
+   p_table = validate_p_table(imagefile);
+
+   /* Get the partition number from argv. */
+   if((part_num = atoi(part)) < 0 || part_num > 3) {
+      fprintf(stderr,"Unexpected partition number\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("Part_num: %d\n",part_num);
+   
+   printf("p_table.lFirst: %d\n",p_table.lFirst);
+   
+   position_offset = p_table.lFirst*SECTOR_SIZE;
+
+
+   /* Get the subpartition number from argv, if applicable. */
+   if(subpart != NULL) {
+
+
+      if((subpart_num = atoi(subpart)) < 0 || subpart_num > 3) {
+         fprintf(stderr,"Undexpected subpartition number\n");
+         exit(EXIT_FAILURE);
+      }
+
+      printf("subpart_num: %d\n",subpart_num);
+   }
+   
+   return position_offset;
 }
+
+
+struct partition_table validate_p_table(FILE *imagefile) {
+   uint16_t signature;
+   struct partition_table p_table;
+   
+   /* Set file position to begining of the partition table */
+   if(fseek(imagefile, P_TABLE_LOCATION, SEEK_SET) != 0) {
+      perror("fseek");
+      exit(errno);
+   }
+   
+   /* Read in partition table struct from imagefile */
+   if(fread(&p_table, sizeof(struct partition_table), 1, imagefile) != 1) {
+      fprintf(stderr, "Unable to read partition table\n");
+      exit(EXIT_FAILURE);
+   }
+   
+   /* Verify this is a valid MINIX partition table type */
+   if(p_table.type != MINIX_PARTITION_TYPE) {
+      fprintf(stderr, "Not a valid MINIX partition type\n");
+      exit(EXIT_FAILURE);
+   }
+
+   /* Verify the partition table signature in bytes 510 and 511 */
+   /* Set file position to the 510th byte. */
+   if(fseek(imagefile, 510, SEEK_SET) != 0) {
+      perror("fseek");
+      exit(errno);
+   }
+   
+   /* Read in byte 510 and 511. */
+   if(fread(&signature, 2*sizeof(char), 1, imagefile) != 1) {
+      fprintf(stderr, "Unable to read bytes 510 and 511\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("signature: %x\n",signature);
+   
+   /* Compare with Minix signature 0x55AA. Flipped for little endianess */
+   if(signature != 0xaa55) {
+      fprintf(stderr,"Unexpected partition table signature.\n");
+      exit(EXIT_FAILURE);
+   }
+   
+   /* If sucessfull, return the table. */
+   return p_table;
+}
+
 
 
 struct superblock validate_superblock(FILE *imagefile) {
@@ -203,15 +286,13 @@ struct superblock validate_superblock(FILE *imagefile) {
    
    /* Set file position to begining of superblock */
    if(fseek(imagefile, position, SEEK_SET) != 0) {
-      perror(NULL);
+      perror("fseek");
       exit(errno);
    }
    
    /* Read in superblock struct from imagefile */
    if(fread(&s_block, sizeof(struct superblock), 1, imagefile) != 1) {
       fprintf(stderr, "Unable to read superblock\n");
-      //fprintf(stderr, "struct superblock size: %d\n",sizeof(struct superblock));
-      //fprintf(stderr, "Read: %d\n",temp);
       exit(EXIT_FAILURE);
    }
    
@@ -221,6 +302,7 @@ struct superblock validate_superblock(FILE *imagefile) {
       exit(EXIT_FAILURE);
    }
 
+   /* If sucessfull, return the superblock. */
    return s_block;
 }
 
